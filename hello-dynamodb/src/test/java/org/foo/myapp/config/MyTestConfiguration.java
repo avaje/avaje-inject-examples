0a173248ce530f155bc0a9c3c912ea64f592c8c4
Rob Bygrave <robin.bygrave@gmail.com>
2022-05-02 16:06:21 +1200

    Update to use ebean-test-docker for localstack test container

--- hello-dynamodb/src/test/java/org/foo/myapp/config/MyTestConfiguration.java
+++ hello-dynamodb/src/test/java/org/foo/myapp/config/MyTestConfiguration.java
@@ -1,18 +1,13 @@
 package org.foo.myapp.config;
 
-import com.amazonaws.auth.AWSStaticCredentialsProvider;
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.model.*;
 import io.avaje.inject.Bean;
 import io.avaje.inject.Factory;
 import io.avaje.inject.test.TestScope;
+import io.ebean.docker.commands.LocalstackContainer;
 import jakarta.inject.Named;
 import org.foo.myapp.HelloData;
-import org.testcontainers.containers.FixedHostPortGenericContainer;
-import org.testcontainers.containers.GenericContainer;
 
 import java.util.List;
 
@@ -23,38 +18,20 @@ import java.util.List;
 @Factory
 class MyTestConfiguration {
 
-  static final String awsRegion = "ap-southeast-2";
-
-  static {
-    System.setProperty("com.amazonaws.sdk.disableCbor", "true");
-    System.setProperty("aws.region", awsRegion);
-    System.setProperty("aws.accesskey", "localstack");
-    System.setProperty("aws.secretkey", "localstack");
-    System.setProperty("stack.name", "rob-test");
-  }
-
   @Named("dynamoContainer")
   @Bean
-  GenericContainer<?> dynamoDBContainer() {
-    GenericContainer<?> container = new FixedHostPortGenericContainer<>("amazon/dynamodb-local:1.13.2")
-      .withFixedExposedPort(9000, 8000)
-      .withEnv("DEFAULT_REGION", awsRegion)
-      .withEnv("AWS_ACCESS_KEY_ID", "localstack")
-      .withEnv("AWS_SECRET_KEY", "localstack")
-      .withCommand("-jar DynamoDBLocal.jar -inMemory -sharedDb");
-
+  LocalstackContainer dynamoDBContainer() {
+    LocalstackContainer container = LocalstackContainer
+      .newBuilder("1.13.2")
+      // .port(4566)
+      .build();
     container.start();
     return container;
   }
 
   @Bean
-  AmazonDynamoDB dynamoDB(GenericContainer<?> dynamoDBContainer) {
-    assert dynamoDBContainer != null;
-    var endpointUrl = String.format("http://localhost:%d", 9000);//dynamoDBContainer.getFirstMappedPort());
-    AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-      .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("localstack", "localstack")))
-      .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(endpointUrl, awsRegion))
-      .build();
+  AmazonDynamoDB dynamoDB(LocalstackContainer dynamoDBContainer) {
+    AmazonDynamoDB client = dynamoDBContainer.dynamoDB();
     createTable(client);
     return client;
   }
@@ -84,6 +61,8 @@ class MyTestConfiguration {
     void createTable(String tableName, List<KeySchemaElement> keys, List<AttributeDefinition> attributes, ProvisionedThroughput throughput) {
       try {
         dynamoDB.createTable(new CreateTableRequest(attributes, tableName, keys, throughput));
+      } catch (ResourceInUseException e) {
+        System.out.println("dynamoDB table already created");
       } catch (Exception e) {
         throw new IllegalStateException(e);
       }
